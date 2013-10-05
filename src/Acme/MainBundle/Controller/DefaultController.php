9696c7c48080dc2de3e9bcc44fe42a12b1ef0f24
AAM <welloffman@yandex.ru>
2013-10-05 12:34:09 +0200

    Доработки

--- src/Acme/MainBundle/Controller/DefaultController.php
+++ src/Acme/MainBundle/Controller/DefaultController.php
@@ -3,14 +3,36 @@
 namespace Acme\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 use Acme\ModelBundle\Entity\User;
 
 class DefaultController extends Controller {
-    public function indexAction() {
-    	return $this->render('AcmeMainBundle:Default:index.html.twig');
-    }
+	public function indexAction() {
+		//$usr = $this->get('security.context')->getToken()->getAttributes();
 
-    public function headerAction() {
-    	return $this->render('AcmeMainBundle:Default:header.html.twig');
-    }
+		//print_r( (int)$this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') );
+		/*if( ){
+			return $this->redirect($this->generateUrl('notes'));
+		}*/
+
+
+		$request = $this->getRequest();
+		$session = $request->getSession();
+
+		if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+			$error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+		} else {
+			$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+			$session->remove(SecurityContext::AUTHENTICATION_ERROR);
+		}
+
+		return $this->render('AcmeMainBundle:Default:index.html.twig', array(
+			'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+			'error' => $error,
+		));
+	}
+
+	public function headerAction() {
+		return $this->render('AcmeMainBundle:Default:header.html.twig');
+	}
 }
